74d6fc38018082e5e5459f22e6496c92f167275e
sungpah <sungpah@gmail.com>
2023-03-11 21:14:53 +0900

    fix iteration error at index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
 
       {
         arr.map((el, index) => {
-          return <div>
+          return <div key={index}>
 
             <div>{el.prompt}</div>
             <div>{el.result}</div>
